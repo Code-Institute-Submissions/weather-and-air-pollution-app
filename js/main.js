68a7de1acc80c225fccb07a806c268e8962208b9
Andrzej <andrzej.rakowski79@gmail.com>
2019-04-14 14:22:01 +0100

    Refactor handle search

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
-$(document).on('keypress', e => {
-  if(e.which == 13) {
-    const cityName = $('#search-txt').val();
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${cityName}&key=AIzaSyA9SZckhP0OZcTgK_8d-bEXjugGkACUBkw`
-    $.getJSON(url, data => {
-      if (data.results.length === 0) {
-        console.log('No data');
-      } else {
-        const {lat, lng} = data.results[0].geometry.location
 
-
-
-      }
-
-    });
+const handleSearch = e => {
+  const enterPressed = e.type === 'keypress' && e.which === 13;
+  const clicked = e.type === 'click';
+  if (enterPressed || clicked) {
+    getGeo($('#search-txt').val())
   };
+}
+
+$(document).on('keypress', handleSearch);
+$('#search-btn').on('click', handleSearch);
 
 
-});
+const getGeo = city => {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyA9SZckhP0OZcTgK_8d-bEXjugGkACUBkw`;
+  $.getJSON(url, data => {
+    const res = data.results
+    if (res.length === 0) {
+      console.log('No data');
+    } else if (res.length > 1) {
+      console.log('More than one results');
+    } else {
+      const {lat, lng} = res[0].geometry.location;
+      console.log({lat});
+
+
+    }
+  });
+}
+
+
+const getAir = () => {
+
+}
+
+const getWeather = () => {
+
+}
